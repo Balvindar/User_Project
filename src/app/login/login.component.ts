8e53e738506f230a2747682b5861c3a00ec84a42
Balvindar <73059145+Balvindar@users.noreply.github.com>
2024-09-01 09:54:07 +0200

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  loginForm!: FormGroup;
+
+  ngOnInit() {
+
+    this.initializeForm();
+  }
+
+
+
+  private initializeForm() {
+
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required)
+    })
+
+  }
+
+  onSubmit() {
+    console.log('Login Form', this.loginForm);
+  }
+
+
+
+}
